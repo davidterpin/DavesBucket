cb4d459c609b2fa8b7b54d9158f52a80184640e6
David Terpin <davidterpin@gmail.com>
2016-03-03 14:31:54 +1100

    Played with more stuff. Check out Matrix

--- app/src/main/java/dev/com/nobatime/davesbucket/ActivityMain.java
+++ app/src/main/java/dev/com/nobatime/davesbucket/ActivityMain.java
@@ -1,13 +1,24 @@
 package dev.com.nobatime.davesbucket;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class ActivityMain extends AppCompatActivity {
+
+
+    ImageView logo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    //    logo =(ImageView) findViewById(R.id.iv_logo);
+     //   Matrix matrix =new Matrix();
+    //    matrix.setRotate(10.0F);
+     //   logo.setImageMatrix(matrix);
+
     }
 }
